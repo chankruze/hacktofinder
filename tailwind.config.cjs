ceb388a46b0e2a61f984f94ed1d97ae0af2d5446
chankruze <chankruze@gmail.com>
2022-10-06 12:15:39 +0530

    🚀 initial commit
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "media",
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          primary: "#222831",
+          secondary: "#393E46",
+          accent: "#FFD369",
+          light: "#EEEEEE",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
+// dark: https://colorhunt.co/palette/222831393e46ffd369eeeeee
