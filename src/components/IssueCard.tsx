be5da23aac6cf410f7e436396026d8a79d73a479
chankruze <chankruze@gmail.com>
2022-10-07 13:48:03 +0530

    fix: align tags to bottom of issue card
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/components/IssueCard.tsx
+++ src/components/IssueCard.tsx
@@ -23,7 +23,7 @@ export const IssueCard: FC<Props> = ({ issue }) => {
   return (
     <div
       className={classNames(
-        "p-4 space-y-3 rounded duration-150",
+        "flex flex-col p-4 space-y-3 rounded duration-150",
         "bg-dark-primary"
       )}
     >
@@ -47,15 +47,15 @@ export const IssueCard: FC<Props> = ({ issue }) => {
       {/* title */}
       <a
         href={issue.url}
-        className="w-fit text-xl font-medium line-clamp-2 text-ellipsis hover:text-blue-400"
+        className="flex-1 w-fit text-xl font-medium line-clamp-2 text-ellipsis hover:text-blue-400"
       >
         {issue.title}{" "}
         <span className="dark:text-dark-accent">
           #{issue.url.split("/")[6]}
         </span>
       </a>
-
-      <div className="flex items-center justify-between flex-wrap gap-2">
+      {/* other info */}
+      <div className="mt-auto flex items-center justify-between flex-wrap gap-2">
         {/* labels */}
         <div className="flex gap-2 flex-wrap">
           {issue.labels.edges.map(({ node: { name } }, idx) => (
